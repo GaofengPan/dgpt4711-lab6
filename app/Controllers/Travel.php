5ead704b0f845e24f162dc4440e0d4c9ad6da7d4
GaofengPan <1254066225@qq.com>
2020-11-11 10:57:52 +0800

    The lab8 work.

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -8,9 +8,35 @@ public function index() {
     $records = $places->findAll();
     // get a template parser
     $parser = \Config\Services::parser(); // tell it about the substitions
-    return $parser->setData(['records' => $records])  
+    //step1
+    $table = new \CodeIgniter\View\Table(); 
+    $headings = $places->fields; 
+    $displayHeadings = array_slice($headings, 1, 2); 
+    $table->setHeading(array_map('ucfirst', $displayHeadings));
+    foreach ($records as $record) { 
+        //$table->addRow($record->name,$record->description); 
+        $nameLink = anchor("travel/showme/$record->id",$record->name); 
+        $table->addRow($nameLink,$record->description); 
+    } 
+    //step2
+    $template = [ 'table_open' => '<table cellpadding="2px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">', ]; 
+    $table->setTemplate($template);
+    $fields = [           
+        'title' => 'Travel Destinations',     
+        'heading' => 'Travel Destinations',         
+        'footer' => 'Copyright GaofengPan'         ];
+    return $parser->setData($fields)               
+                    ->render('templates\top') .      
+            $table->generate() .          
+            $parser->setData($fields)
+                    ->render('templates\bottom');
+    
+    
+    /*return $parser->setData(['records' => $records])  
 // and have it render the template with those  
-        ->render('placeslist');
+        ->render('placeslist');*/
     }
 public function showme($id)
 { 
@@ -21,9 +47,37 @@ public function showme($id)
     // get a template parser
     $parser = \Config\Services::parser();
     // tell it about the substitions
-    return $parser->setData($record)  
+    
+    //step1
+    $table = new \CodeIgniter\View\Table(); 
+    $headings = $places->fields;
+    $headings=array_map('ucfirst', $headings);
+    $table->addRow($headings[0],$record["id"]);
+    $table->addRow($headings[1],$record["name"]);
+    $table->addRow($headings[2],$record["description"]);
+    $table->addRow($headings[3],$record["link"]);
+    $table->addRow($headings[4],"<img src='/image/".$record['image'].'\'/>');
+    
+     //step2
+    $template = [ 'table_open' => '<table cellpadding="2px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">', ]; 
+    $table->setTemplate($template);
+    $fields = [           
+        'title' => 'Travel Destinations',     
+        'heading' => 'Travel Destinations',         
+        'footer' => 'Copyright GaofengPan'         
+        ];
+    return $parser->setData($fields)               
+                    ->render('templates\top') .      
+            $table->generate() .          
+            $parser->setData($fields)
+                    ->render('templates\bottom');
+    
+    
+    /*return $parser->setData($fields)
 // and have it render the template with those  
-        ->render('oneplace');
+        ->render('oneplace');*/
 }
   
 }
